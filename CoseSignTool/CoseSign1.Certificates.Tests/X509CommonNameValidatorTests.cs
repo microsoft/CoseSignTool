89bfb47d2b2cd8f9c9713715b889b81a5e8d8bf9
lemccomb <117306942+lemccomb@users.noreply.github.com>
2023-09-07 17:15:46 -0700

    Add license text to code files

--- CoseSignTool/CoseSign1.Certificates.Tests/X509CommonNameValidatorTests.cs
+++ CoseSignTool/CoseSign1.Certificates.Tests/X509CommonNameValidatorTests.cs
@@ -1,8 +1,5 @@
-﻿// ---------------------------------------------------------------------------
-// <copyright file="X509CommonNameValidatorTests.cs" company="Microsoft">
-//     Copyright (c) Microsoft Corporation. All rights reserved.
-// </copyright>
-// ---------------------------------------------------------------------------
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 namespace CoseSign1.Certificates.Tests;
 
--- CoseSignTool/CoseSign1.Certificates/Exceptions/CoseValidationException.cs
+++ CoseSignTool/CoseSign1.Certificates/Exceptions/CoseValidationException.cs
@@ -1,8 +1,5 @@
-﻿// ---------------------------------------------------------------------------
-// <copyright file="CoseValidationException.cs" company="Microsoft">
-//     Copyright (c) Microsoft Corporation. All rights reserved.
-// </copyright>
-// ---------------------------------------------------------------------------
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 namespace CoseSign1.Certificates.Exceptions;
 

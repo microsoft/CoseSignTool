1fde7e6338de5adfb1cbf79596d62d0ca6859aab
Jeromy Statia <jstatia@microsoft.com>
2023-11-09 09:47:31 -0800

    Pull CoseDetachedSignatures into a dedicated project with nuget dependencies on CoseSign1 nuget package to reduce confusion

---
+++ CoseDetachedSIgnature.Tests/Usings.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+global using System.Security.Cryptography;
+global using System.Security.Cryptography.Cose;
+global using System.Security.Cryptography.X509Certificates;
+global using CoseDetachedSignature;
+global using CoseDetachedSignature.Extensions;
+global using CoseSign1;
+global using CoseSign1.Abstractions.Interfaces;
+global using CoseSign1.Interfaces;
+global using CoseSign1.Tests.Common;
+global using FluentAssertions;
+global using Moq;
+global using NUnit.Framework;
+
---
+++ CoseDetachedSignature/Usings.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+global using System;
+global using System.Collections.Concurrent;
+global using System.Collections.Generic;
+global using System.Diagnostics;
+global using System.IO;
+global using System.Linq;
+global using System.Reflection;
+global using System.Security.Cryptography;
+global using System.Security.Cryptography.Cose;
+global using System.Text.RegularExpressions;
+global using System.Threading.Tasks;
+global using CoseDetachedSignature.Extensions;
+global using CoseSign1.Abstractions.Interfaces;
+global using CoseSign1.Extensions;
+global using CoseSign1.Interfaces;
+global using CoseSign1;

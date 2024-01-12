4e28fa8d18fb87e610d2f48f89d86d5569d3f91d
GitHub Action <action@github.com>
2024-01-12 12:44:01 -0800

    revert use of primary constructor for CoseValidationError
    Reverting because the primary structure feature is not supported on all frameworks that support .NET Standard 2.0

--- CoseHandler/CoseValidationError.cs
+++ CoseHandler/CoseValidationError.cs
@@ -8,19 +8,28 @@ using System.Collections.Generic;
 /// <summary>
 /// Holds information about an error in COSE validation.
 /// </summary>
-/// <param name="errorCode">A ValidationFailureCode value that represents the error type.</param>
-
-public readonly struct CoseValidationError(ValidationFailureCode errorCode)
+public readonly struct CoseValidationError
 {
+    /// <summary>
+    /// Creates a new CoseValidationError instance.
+    /// </summary>
+    /// <param name="errorCode">A ValidationFailureCode value that represents the error type.</param>
+    /// <param name="message">A text description of the error.</param>
+    public CoseValidationError(ValidationFailureCode errorCode)
+    {
+        ErrorCode = errorCode;
+        Message = ErrorMessages[errorCode];
+    }
+
     /// <summary>
     /// Gets or sets a ValidationFailureCode value that represents the error type.
     /// </summary>
-    public ValidationFailureCode ErrorCode { get; } = errorCode;
+    public ValidationFailureCode ErrorCode { get; }
 
     /// <summary>
     /// Gets or sets a text description of the error.
     /// </summary>
-    public string Message { get; } = ErrorMessages[errorCode];
+    public string Message { get; }
 
     /// <summary>
     /// A dictionary that maps error messages to error codes.
@@ -32,12 +41,10 @@ public readonly struct CoseValidationError(ValidationFailureCode errorCode)
         { ValidationFailureCode.NoPublicKey, "No public key could be found for the signing certificate."},
         { ValidationFailureCode.CertificateChainUnreadable, "One or more certificates in the certificate chain could not be read."},
         { ValidationFailureCode.CertificateChainInvalid, "Certificate chain validation failed." },
-        { ValidationFailureCode.TrustValidationFailed, "The signature failed to validate against the trust validator." },
         { ValidationFailureCode.PayloadMismatch, "The supplied or embedded payload does not match the hash of the payload that was signed." },
         { ValidationFailureCode.PayloadMissing, "The detached signature could not be validated because the original payload was nut supplied."},
         { ValidationFailureCode.PayloadUnreadable, "The payload content could not be read."},
         { ValidationFailureCode.RedundantPayload, "The embedded signature was not validated because external payload was also specified."},
         { ValidationFailureCode.CoseHeadersInvalid, "The COSE headers in the signature could not be read." },
-        { ValidationFailureCode.Unknown, "An unknown error was thrown." }
     };
 }

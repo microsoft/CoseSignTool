e591076ca07fabc69444862d1af2936cceef8a76
lemccomb <117306942+lemccomb@users.noreply.github.com>
2024-05-24 11:59:57 -0700

    Delete CoseSignTool.tests directory
    This directory name is incorrectly cased.
    
    Signed-off-by: lemccomb <117306942+lemccomb@users.noreply.github.com>

--- CoseSignTool.tests/GetCommandTests.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-namespace CoseSignTool.Tests;
-
-internal class GetCommandTests
-{
-    // Placeholder for tests for the GetCommand class.
-}

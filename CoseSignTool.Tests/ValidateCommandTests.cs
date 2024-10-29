dd7a5baf2088c52983786e8823bfaf8bc3c41691
Edwin Lantigua <elantigua@microsoft.com>
2024-10-29 00:08:28 -0700

    oops

--- CoseSignTool.Tests/ValidateCommandTests.cs
+++ CoseSignTool.Tests/ValidateCommandTests.cs
@@ -253,7 +253,7 @@ public class ValidateCommandTests
             // https://github.com/NuGet/Home/issues/11985
             // OSX no longer trusts CRLs and will fail validation on any chain that lacks OCSPs
             X509RevocationMode revocationMode = RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ?
-                X509RevocationMode.NoCheck :
+                X509RevocationMode.Offline :
                 X509RevocationMode.Online;
 
             // setup validator
@@ -262,7 +262,7 @@ public class ValidateCommandTests
                 coseStream,
                 new FileInfo(Path.Combine(TestData, "payload.json")),
                 [root],
-                X509RevocationMode.Online);
+                revocationMode);
             Console.WriteLine(result.ToString(true, true));
 
             result.Success.Should().BeTrue(result.ToString(true, true));

781234220fc8f8aa0f2ed0ecdabd877120e9a742
Jeromy Statia (from Dev Box) <jstatia@microsoft.com>
2025-06-05 12:17:04 -0700

    Add unit tests for CoseSign1MessageExtensions and enhance CertificateCoseHeaderLabels accessibility

--- CoseSign1.Certificates/CertificateCoseHeaderLabels.cs
+++ CoseSign1.Certificates/CertificateCoseHeaderLabels.cs
@@ -6,19 +6,21 @@ namespace CoseSign1.Certificates;
 /// <summary>
 /// <see cref="CoseHeaderLabel"/> objects which are specific to certificate signed <see cref="CoseSign1Message"/> objects.
 /// </summary>
-internal class CertificateCoseHeaderLabels
+public static class CertificateCoseHeaderLabels
 {
     // Taken from https://www.iana.org/assignments/cose/cose.xhtml
     /// <summary>
     /// Represents an unordered list of certificates.
     /// </summary>
-    internal static readonly CoseHeaderLabel X5Bag = new(32);
+    public static readonly CoseHeaderLabel X5Bag = new(32);
+
     /// <summary>
     /// Represents an ordered list (leaf first) of the certificate chain for the certificate used to sign the <see cref="CoseSign1Message"/> object.
     /// </summary>
-    internal static readonly CoseHeaderLabel X5Chain = new(33);
+    public static readonly CoseHeaderLabel X5Chain = new(33);
+
     /// <summary>
     /// Represents the thumbprint for the certificate used to sign the <see cref="CoseSign1Message"/> object.
     /// </summary>
-    internal static readonly CoseHeaderLabel X5T = new(34);
+    public static readonly CoseHeaderLabel X5T = new(34);
 }

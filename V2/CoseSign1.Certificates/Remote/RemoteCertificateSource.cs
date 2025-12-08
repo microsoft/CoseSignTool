f177359a9d00caccdf206db64382512e109d3953
Jeromy Statia (from Dev Box) <jstatia@microsoft.com>
2025-12-08 12:59:14 -0800

    Add remote signing service abstractions for RSA/ECDSA/ML-DSA
    - RemoteCertificateSource: Abstract base class with signing method contracts
      - Provides RSA (PS256/384/512), ECDSA (ES256/384/512), ML-DSA (44/65/87) signing
      - Includes sync/async variants for SignData and SignHash operations
      - Uses OID-based key sizing for ML-DSA (KeySize property doesn't exist)
    
    - RemoteSigningKeyProvider: ISigningKey implementation wrapping RemoteCertificateSource
      - Automatic COSE algorithm ID detection based on public key OID and size
      - Proper SigningKeyMetadata creation with CryptographicKeyType enum
      - Thread-safe CoseKey generation and caching
    
    - Architecture enables remote signing services (Azure Key Vault, Trusted Signing, HSM) to implement abstract signing methods
    - ML-DSA support backported to netstandard2.0 via System.Security.Cryptography packages
    - SYSLIB5006 warnings suppressed for preview ML-DSA APIs
    
    All 248 V2 tests passing

---
+++ V2/CoseSign1.Certificates/Remote/RemoteCertificateSource.cs
@@ -0,0 +1,193 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using CoseSign1.Certificates.ChainBuilders;
+using CoseSign1.Certificates.Interfaces;
+
+namespace CoseSign1.Certificates.Remote;
+
+/// <summary>
+/// Abstract base class for remote certificate sources that perform signing operations
+/// via a remote service (e.g., Azure Key Vault, Azure Trusted Signing, HSM, etc.).
+/// </summary>
+/// <remarks>
+/// Remote signing services must implement the signing operations for RSA, ECDSA, and ML-DSA algorithms.
+/// Authentication, service connection, and service-specific configuration are the responsibility of the derived class.
+/// </remarks>
+public abstract class RemoteCertificateSource : CertificateSourceBase
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RemoteCertificateSource"/> class.
+    /// </summary>
+    /// <param name="chainBuilder">Optional custom chain builder. If null, uses X509ChainBuilder for automatic chain building.</param>
+    protected RemoteCertificateSource(ICertificateChainBuilder? chainBuilder = null)
+        : base(chainBuilder ?? new X509ChainBuilder())
+    {
+    }
+
+    /// <inheritdoc/>
+    public override bool HasPrivateKey => true; // Remote services always have access to private key operations
+
+    /// <summary>
+    /// Signs data using RSA with the specified hash algorithm and padding.
+    /// </summary>
+    /// <param name="data">The data to sign.</param>
+    /// <param name="hashAlgorithm">The hash algorithm to use.</param>
+    /// <param name="padding">The RSA signature padding mode.</param>
+    /// <returns>The signature bytes.</returns>
+    /// <exception cref="NotSupportedException">Thrown if the remote service does not support RSA signing.</exception>
+    public abstract byte[] SignDataWithRsa(byte[] data, HashAlgorithmName hashAlgorithm, RSASignaturePadding padding);
+
+    /// <summary>
+    /// Signs data using RSA with the specified hash algorithm and padding asynchronously.
+    /// </summary>
+    /// <param name="data">The data to sign.</param>
+    /// <param name="hashAlgorithm">The hash algorithm to use.</param>
+    /// <param name="padding">The RSA signature padding mode.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the signature bytes.</returns>
+    /// <exception cref="NotSupportedException">Thrown if the remote service does not support RSA signing.</exception>
+    public abstract Task<byte[]> SignDataWithRsaAsync(byte[] data, HashAlgorithmName hashAlgorithm, RSASignaturePadding padding, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Signs a hash using RSA with the specified hash algorithm and padding.
+    /// </summary>
+    /// <param name="hash">The hash to sign.</param>
+    /// <param name="hashAlgorithm">The hash algorithm that was used to create the hash.</param>
+    /// <param name="padding">The RSA signature padding mode.</param>
+    /// <returns>The signature bytes.</returns>
+    /// <exception cref="NotSupportedException">Thrown if the remote service does not support RSA signing.</exception>
+    public abstract byte[] SignHashWithRsa(byte[] hash, HashAlgorithmName hashAlgorithm, RSASignaturePadding padding);
+
+    /// <summary>
+    /// Signs a hash using RSA with the specified hash algorithm and padding asynchronously.
+    /// </summary>
+    /// <param name="hash">The hash to sign.</param>
+    /// <param name="hashAlgorithm">The hash algorithm that was used to create the hash.</param>
+    /// <param name="padding">The RSA signature padding mode.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the signature bytes.</returns>
+    /// <exception cref="NotSupportedException">Thrown if the remote service does not support RSA signing.</exception>
+    public abstract Task<byte[]> SignHashWithRsaAsync(byte[] hash, HashAlgorithmName hashAlgorithm, RSASignaturePadding padding, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Signs data using ECDSA with the specified hash algorithm.
+    /// </summary>
+    /// <param name="data">The data to sign.</param>
+    /// <param name="hashAlgorithm">The hash algorithm to use.</param>
+    /// <returns>The signature bytes in IEEE P1363 format.</returns>
+    /// <exception cref="NotSupportedException">Thrown if the remote service does not support ECDSA signing.</exception>
+    public abstract byte[] SignDataWithEcdsa(byte[] data, HashAlgorithmName hashAlgorithm);
+
+    /// <summary>
+    /// Signs data using ECDSA with the specified hash algorithm asynchronously.
+    /// </summary>
+    /// <param name="data">The data to sign.</param>
+    /// <param name="hashAlgorithm">The hash algorithm to use.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the signature bytes in IEEE P1363 format.</returns>
+    /// <exception cref="NotSupportedException">Thrown if the remote service does not support ECDSA signing.</exception>
+    public abstract Task<byte[]> SignDataWithEcdsaAsync(byte[] data, HashAlgorithmName hashAlgorithm, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Signs a hash using ECDSA.
+    /// </summary>
+    /// <param name="hash">The hash to sign.</param>
+    /// <returns>The signature bytes in IEEE P1363 format.</returns>
+    /// <exception cref="NotSupportedException">Thrown if the remote service does not support ECDSA signing.</exception>
+    public abstract byte[] SignHashWithEcdsa(byte[] hash);
+
+    /// <summary>
+    /// Signs a hash using ECDSA asynchronously.
+    /// </summary>
+    /// <param name="hash">The hash to sign.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the signature bytes in IEEE P1363 format.</returns>
+    /// <exception cref="NotSupportedException">Thrown if the remote service does not support ECDSA signing.</exception>
+    public abstract Task<byte[]> SignHashWithEcdsaAsync(byte[] hash, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Signs data using ML-DSA (Module-Lattice-Based Digital Signature Algorithm).
+    /// </summary>
+    /// <param name="data">The data to sign.</param>
+    /// <param name="hashAlgorithm">The hash algorithm to use for pre-hashing (optional, depending on ML-DSA variant).</param>
+    /// <returns>The signature bytes.</returns>
+    /// <exception cref="NotSupportedException">Thrown if the remote service does not support ML-DSA signing.</exception>
+    /// <remarks>
+    /// ML-DSA (FIPS 204) is a post-quantum signature algorithm.
+    /// </remarks>
+    public abstract byte[] SignDataWithMLDsa(byte[] data, HashAlgorithmName? hashAlgorithm = null);
+
+    /// <summary>
+    /// Signs data using ML-DSA (Module-Lattice-Based Digital Signature Algorithm) asynchronously.
+    /// </summary>
+    /// <param name="data">The data to sign.</param>
+    /// <param name="hashAlgorithm">The hash algorithm to use for pre-hashing (optional, depending on ML-DSA variant).</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the signature bytes.</returns>
+    /// <exception cref="NotSupportedException">Thrown if the remote service does not support ML-DSA signing.</exception>
+    /// <remarks>
+    /// ML-DSA (FIPS 204) is a post-quantum signature algorithm.
+    /// </remarks>
+    public abstract Task<byte[]> SignDataWithMLDsaAsync(byte[] data, HashAlgorithmName? hashAlgorithm = null, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Gets the public key algorithm identifier for the signing certificate.
+    /// This is used to determine which signing method to use.
+    /// </summary>
+    /// <returns>The OID string for the public key algorithm (e.g., "1.2.840.113549.1.1.1" for RSA).</returns>
+    protected string GetPublicKeyAlgorithm()
+    {
+        var cert = GetSigningCertificate();
+        return cert.GetKeyAlgorithm();
+    }
+
+    /// <summary>
+    /// Gets the key size in bits for the signing certificate.
+    /// </summary>
+    /// <returns>The key size in bits.</returns>
+    protected int GetKeySize()
+    {
+        var cert = GetSigningCertificate();
+        
+        // Try to get the public key and determine its size
+        using var rsa = cert.GetRSAPublicKey();
+        if (rsa != null)
+        {
+            return rsa.KeySize;
+        }
+
+        using var ecdsa = cert.GetECDsaPublicKey();
+        if (ecdsa != null)
+        {
+            return ecdsa.KeySize;
+        }
+
+#pragma warning disable SYSLIB5006 // ML-DSA APIs are marked as preview in .NET 10
+        try
+        {
+            using var mldsa = cert.GetMLDsaPublicKey();
+            if (mldsa != null)
+            {
+                // ML-DSA KeySize property doesn't exist, use OID-based sizing
+                var publicKeyOid = cert.PublicKey.Oid.Value;
+                return publicKeyOid switch
+                {
+                    "2.16.840.1.101.3.4.3.17" => 44,  // ML-DSA-44
+                    "2.16.840.1.101.3.4.3.18" => 65,  // ML-DSA-65
+                    "2.16.840.1.101.3.4.3.19" => 87,  // ML-DSA-87
+                    _ => 65 // Default to ML-DSA-65
+                };
+            }
+        }
+        catch
+        {
+            // ML-DSA might not be available or supported
+        }
+#pragma warning restore SYSLIB5006
+
+        throw new NotSupportedException($"Unable to determine key size for certificate with algorithm {cert.GetKeyAlgorithm()}");
+    }
+}
---
+++ V2/CoseSign1.Certificates/Remote/RemoteSigningKeyProvider.cs
@@ -0,0 +1,215 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Security.Cryptography;
+using System.Security.Cryptography.Cose;
+using System.Security.Cryptography.X509Certificates;
+using CoseSign1.Abstractions;
+
+namespace CoseSign1.Certificates.Remote;
+
+/// <summary>
+/// Provides signing key operations using a remote certificate source.
+/// Automatically detects the key type (RSA, ECDSA, ML-DSA) and uses the appropriate signing method.
+/// </summary>
+public class RemoteSigningKeyProvider : ISigningKey
+{
+    private readonly RemoteCertificateSource _certificateSource;
+    private readonly Lazy<SigningKeyMetadata> _metadata;
+    private CoseKey? _coseKey;
+    private readonly object _coseKeyLock = new();
+    private bool _disposed;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RemoteSigningKeyProvider"/> class.
+    /// </summary>
+    /// <param name="certificateSource">The remote certificate source that provides the signing operations.</param>
+    /// <param name="signingService">The signing service that owns this key.</param>
+    public RemoteSigningKeyProvider(RemoteCertificateSource certificateSource, ISigningService signingService)
+    {
+#if NET5_0_OR_GREATER
+        ArgumentNullException.ThrowIfNull(certificateSource);
+        ArgumentNullException.ThrowIfNull(signingService);
+#else
+        if (certificateSource == null) { throw new ArgumentNullException(nameof(certificateSource)); }
+        if (signingService == null) { throw new ArgumentNullException(nameof(signingService)); }
+#endif
+        _certificateSource = certificateSource;
+        SigningService = signingService;
+        _metadata = new Lazy<SigningKeyMetadata>(() => CreateMetadata());
+    }
+
+    /// <inheritdoc/>
+    public SigningKeyMetadata Metadata => _metadata.Value;
+
+    /// <inheritdoc/>
+    public ISigningService SigningService { get; }
+
+    /// <inheritdoc/>
+    public CoseKey GetCoseKey()
+    {
+        if (_coseKey != null)
+        {
+            return _coseKey;
+        }
+
+        lock (_coseKeyLock)
+        {
+            if (_coseKey != null)
+            {
+                return _coseKey;
+            }
+
+            _coseKey = CreateCoseKeyForRemote();
+            return _coseKey;
+        }
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        _coseKey = null;
+        _disposed = true;
+        GC.SuppressFinalize(this);
+    }
+
+    private SigningKeyMetadata CreateMetadata()
+    {
+        var cert = _certificateSource.GetSigningCertificate();
+        var publicKeyOid = cert.PublicKey.Oid.Value;
+
+        // RSA: 1.2.840.113549.1.1.1
+        if (publicKeyOid == "1.2.840.113549.1.1.1")
+        {
+            using var rsa = cert.GetRSAPublicKey();
+            if (rsa != null)
+            {
+                int coseAlgorithmId = rsa.KeySize switch
+                {
+                    >= 4096 => -39, // PS512
+                    >= 3072 => -38, // PS384
+                    _ => -37        // PS256
+                };
+                
+                var hashAlgorithm = coseAlgorithmId switch
+                {
+                    -39 => HashAlgorithmName.SHA512,
+                    -38 => HashAlgorithmName.SHA384,
+                    _ => HashAlgorithmName.SHA256
+                };
+
+                return new SigningKeyMetadata(
+                    coseAlgorithmId: coseAlgorithmId,
+                    keyType: CryptographicKeyType.RSA,
+                    isRemote: true,
+                    hashAlgorithm: hashAlgorithm,
+                    keySizeInBits: rsa.KeySize,
+                    additionalMetadata: null);
+            }
+        }
+
+        // ECDSA: 1.2.840.10045.2.1
+        if (publicKeyOid == "1.2.840.10045.2.1")
+        {
+            using var ecdsa = cert.GetECDsaPublicKey();
+            if (ecdsa != null)
+            {
+                int coseAlgorithmId = ecdsa.KeySize switch
+                {
+                    521 => -36, // ES512 (P-521)
+                    384 => -35, // ES384 (P-384)
+                    _ => -7     // ES256 (P-256)
+                };
+                
+                var hashAlgorithm = coseAlgorithmId switch
+                {
+                    -36 => HashAlgorithmName.SHA512,
+                    -35 => HashAlgorithmName.SHA384,
+                    _ => HashAlgorithmName.SHA256
+                };
+
+                return new SigningKeyMetadata(
+                    coseAlgorithmId: coseAlgorithmId,
+                    keyType: CryptographicKeyType.ECDsa,
+                    isRemote: true,
+                    hashAlgorithm: hashAlgorithm,
+                    keySizeInBits: ecdsa.KeySize,
+                    additionalMetadata: null);
+            }
+        }
+
+        // ML-DSA-44: 2.16.840.1.101.3.4.3.17
+        // ML-DSA-65: 2.16.840.1.101.3.4.3.18
+        // ML-DSA-87: 2.16.840.1.101.3.4.3.19
+        if (publicKeyOid?.StartsWith("2.16.840.1.101.3.4.3.") == true)
+        {
+            (int coseAlgorithmId, int? keySizeInBits, HashAlgorithmName hashAlgorithm) = publicKeyOid switch
+            {
+                "2.16.840.1.101.3.4.3.17" => (-48, (int?)44, HashAlgorithmName.SHA256),  // ML-DSA-44
+                "2.16.840.1.101.3.4.3.18" => (-49, (int?)65, HashAlgorithmName.SHA384),  // ML-DSA-65
+                "2.16.840.1.101.3.4.3.19" => (-50, (int?)87, HashAlgorithmName.SHA512),  // ML-DSA-87
+                _ => (-48, (int?)null, HashAlgorithmName.SHA256) // Default
+            };
+
+            return new SigningKeyMetadata(
+                coseAlgorithmId: coseAlgorithmId,
+                keyType: CryptographicKeyType.MLDSA,
+                isRemote: true,
+                hashAlgorithm: hashAlgorithm,
+                keySizeInBits: keySizeInBits,
+                additionalMetadata: new Dictionary<string, object>
+                {
+                    ["PublicKeyAlgorithmOid"] = publicKeyOid ?? "unknown"
+                });
+        }
+
+        throw new NotSupportedException($"Unsupported key algorithm OID: {publicKeyOid}");
+    }
+
+    private CoseKey CreateCoseKeyForRemote()
+    {
+        var cert = _certificateSource.GetSigningCertificate();
+        var publicKeyOid = cert.PublicKey.Oid.Value;
+
+        // RSA: 1.2.840.113549.1.1.1
+        if (publicKeyOid == "1.2.840.113549.1.1.1")
+        {
+            using var rsa = cert.GetRSAPublicKey();
+            if (rsa != null)
+            {
+                var hashAlgorithm = Metadata.HashAlgorithm ?? HashAlgorithmName.SHA256;
+                return new CoseKey(rsa, RSASignaturePadding.Pss, hashAlgorithm);
+            }
+        }
+
+        // ECDSA: 1.2.840.10045.2.1
+        if (publicKeyOid == "1.2.840.10045.2.1")
+        {
+            using var ecdsa = cert.GetECDsaPublicKey();
+            if (ecdsa != null)
+            {
+                var hashAlgorithm = Metadata.HashAlgorithm ?? HashAlgorithmName.SHA256;
+                return new CoseKey(ecdsa, hashAlgorithm);
+            }
+        }
+
+        // ML-DSA
+        if (publicKeyOid?.StartsWith("2.16.840.1.101.3.4.3.") == true)
+        {
+#pragma warning disable SYSLIB5006 // ML-DSA APIs are marked as preview in .NET 10
+            using var mldsa = cert.GetMLDsaPublicKey();
+            if (mldsa != null)
+            {
+                return new CoseKey(mldsa);
+            }
+#pragma warning restore SYSLIB5006
+        }
+
+        throw new NotSupportedException($"Unable to create CoseKey for algorithm OID {publicKeyOid}");
+    }
+}

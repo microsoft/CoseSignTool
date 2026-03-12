aae26725fede76e2f96cd4d8c7fefba5e8ff111e
Max <maxtropets@gmail.com>
2026-03-12 07:26:44 -0700

    Simplify COSE OpenSSL (#169)

--- native/rust/cose_openssl/src/lib.rs
+++ native/rust/cose_openssl/src/lib.rs
@@ -1,9 +1,11 @@
+mod cbor;
 mod cose;
 mod ossl_wrappers;
 mod sign;
 mod verify;
 
-pub use cose::{cose_sign1, cose_verify1};
+pub use cbor::CborValue;
+pub use cose::{cose_sign1, cose_sign1_encoded, cose_verify1};
 pub use ossl_wrappers::{EvpKey, KeyType, WhichEC};
 
 #[cfg(feature = "pqc")]
